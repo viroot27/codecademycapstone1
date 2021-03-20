5f9902ed8689f65edbddd69724d9a8c454f8d33b
viroot27 <virattalwar@gmail.com>
2021-03-20 11:13:00 -0400

    created intro and deck fnty

--- blackjack.py
+++ blackjack.py
@@ -1,1 +1,18 @@
-print("hello world")
+from random import randint, choice
+
+#Create a Class for Card
+
+intro = "Welcome to Blackjack, to play, please type 'play'."
+
+#deck of card dict
+deck = {
+    "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]
+    }
+
+
+
+print(intro)
+
+if input(intro) == "play":
+    #assign random card
+    pass
