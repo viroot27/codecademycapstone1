1d3b7b6c5155acf27f7c43fcc75c38341b221da8
viroot27 <virattalwar@gmail.com>
2021-03-23 11:42:53 -0400

    added logic for deck creation and card printing

--- blackjack.py
+++ blackjack.py
@@ -1,23 +1,36 @@
 from random import randint, choice
 
-#Create a Class for Card
+#Card class
 class Card:
-    def __init__(self, name, value):
-        self.name = name
+    def __init__(self, card, value):
+        self.card = card
         self.value = value
-        
+
+
+max_hand = 21
+
 
 intro = "Welcome to Blackjack, to play, please type 'play'."
 
-#deck of card dict
-deck = {
-    "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": [1, 11]
+#deck creation logic
+suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+values= {
+   "A": 11, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10
     }
+deck = []
+for suit in suits:
+    for card in cards:
+        deck.append(Card(card, values[card]))
 
 
 
-print(intro)
 
-if input(intro) == "play":
+print(deck)
+#print(intro)
+
+#if input(intro) == "play":
     #assign random card
-    pass
+        
+
+
