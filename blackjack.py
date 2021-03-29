28910480135629b0009f2776753b525cf4ca9b12
viroot27 <virattalwar@gmail.com>
2021-03-29 11:26:49 -0400

    indent

--- blackjack.py
+++ blackjack.py
@@ -143,29 +143,29 @@ def blackjack_game(deck):
     player_score = 0
     dealer_score = 0
     
-#first phase of dealing
-while len(player_cards) <= 2:
+    #first phase of dealing
+    while len(player_cards) <= 2:
 
-    #randomly deal player a card
-    player_card = choice(deck)
-    player_cards.append(player_card)
-    deck.remove(player_card)
+        #randomly deal player a card
+        player_card = choice(deck)
+        player_cards.append(player_card)
+        deck.remove(player_card)
 
 
-    #update the player's score
-    player_score += player_card.value
+        #update the player's score
+        player_score += player_card.value
 
-    #if both are Ace, make sure the first card is stored as one
-    if len(player_cards) == 2:
-        if player_cards[0].value == 11 and player_cards[1].value == 11:
-            player_cards[0].card_value = 1
-            player_score -= 10
+        #if both are Ace, make sure the first card is stored as one
+        if len(player_cards) == 2:
+            if player_cards[0].value == 11 and player_cards[1].value == 11:
+                player_cards[0].card_value = 1
+                player_score -= 10
 
-    #print cards and score
-    print("Player's Cards: ")
-    print_cards(player_cards, False)
-    print("Player's Score: ", player_score)
-    
+        #print cards and score
+        print("Player's Cards: ")
+        print_cards(player_cards, False)
+        print("Player's Score: ", player_score)
+
 
 
 
